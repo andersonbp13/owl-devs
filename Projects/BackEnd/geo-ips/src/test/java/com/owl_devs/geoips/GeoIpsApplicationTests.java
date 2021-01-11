37e872b9588c8d29e3cd58b13b276519f0e2b6b4
andersonbp13 <andersonbp13@hotmail.com>
2021-01-10 21:49:23 -0500

    project initialized

---
+++ Projects/BackEnd/geo-ips/src/test/java/com/owl_devs/geoips/GeoIpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl_devs.geoips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoIpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
